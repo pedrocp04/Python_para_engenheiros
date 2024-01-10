f9e4a6954b1d2505a3080233648fb3a7fe3f2598
Pedro Pigatto <pedropigatto54@gmail.com>
2024-01-10 11:18:30 -0300

    Update combinação.py
    Deixa o codigo mais limpo e compacto, assim facilita a leitura

--- combinação.py
+++ combinação.py
@@ -1,28 +1,29 @@
-lista_de_letras = ['A','R','A','R','A']
-
-lista_de_palavras =[ ]
-
+lista_de_letras = ['A', 'R', 'A', 'R', 'A']
+lista_de_palavras = []
 iteracoes = 0
 
-for i5 in range(len(lista_de_letras)):
-    L5 = lista_de_letras[i5]
+for i5, L5 in enumerate(lista_de_letras):
     Lista_L5 = lista_de_letras.copy()
-    Lista_L4 = Lista_L5.copy()
-    Lista_L4.pop(i5)
-    for i4 in range(len(Lista_L4)):
-        L4 = Lista_L4[i4]
-        Lista_L3 = Lista_L4.copy()
-        Lista_L3.pop(i4)
-        for i3 in range(len(Lista_L3)):
-            L3 = Lista_L3[i3]
-            Lista_L2 = Lista_L3.copy()
-            Lista_L2.pop(i3)
-            for i2 in range(len(Lista_L2)):
-                L2 = Lista_L2[i2]
-                Lista_L1 = Lista_L2.copy()
-                Lista_L1.pop(i2)
-                for i1 in range(len(Lista_L1)):
-                    L1 = Lista_L1[i1]
+    Lista_L5.pop(i5)
+
+    for i4, L4 in enumerate(Lista_L5):
+        Lista_L4 = Lista_L5.copy()
+        Lista_L4.pop(i4)
+
+        for i3, L3 in enumerate(Lista_L4):
+            Lista_L3 = Lista_L4.copy()
+            Lista_L3.pop(i3)
+
+            for i2, L2 in enumerate(Lista_L3):
+                Lista_L2 = Lista_L3.copy()
+                Lista_L2.pop(i2)
+
+                for i1, L1 in enumerate(Lista_L2):
                     palavra = L5 + L4 + L3 + L2 + L1
+                    iteracoes += 1
+
                     if palavra not in lista_de_palavras:
                         lista_de_palavras.append(palavra)
+
+print(iteracoes)
+print(lista_de_palavras)
